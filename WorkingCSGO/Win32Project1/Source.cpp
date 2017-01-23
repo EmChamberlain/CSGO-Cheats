a281f0e7452c7de77deb0fac21dc5549853b54ef
MattChamberlain <Mattchamberlain2@gmail.com>
2017-01-22 21:46:37 -0500

    Updated offsets

--- WorkingCSGO/Win32Project1/Source.cpp
+++ WorkingCSGO/Win32Project1/Source.cpp
@@ -57,10 +57,10 @@ float YawMaxPunch = 2.f;
 
 
 //these tend to change*********************************** Last realistic date that was ban flagged 07-23-15
-const DWORD playerBase = 0x00AA5834;
-const DWORD entityBase = 0x04AC8014;
-const DWORD viewMat1Offset = 0x04AB9BB4;
-const DWORD enginePositionOffset = 0x005C7524;//also called dwClientState or EnginePointer; 0x006BD9BC
+const DWORD playerBase = 0x00AA6818;
+const DWORD entityBase = 0x04AC90F4;
+const DWORD viewMat1Offset = 0x04ABAC94;
+const DWORD enginePositionOffset = 0x005C75A4;//also called dwClientState or EnginePointer; 0x006BD9BC
 //const DWORD viewMat2Offset = 0x04A2AFE4;//not updated
 //const DWORD viewMat3Offset = 0x04A2B2F4;//not updated 
 //******************************************************* 
